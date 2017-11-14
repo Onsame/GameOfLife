b653a7ecce6a0c05a836863e42e73c56752b20c5
user <tyler.w.wagner@edu.sait.ca>
2017-11-14 16:08:20 -0700

    OpenGL Programs

---
+++ OpenGL/square.c
@@ -0,0 +1,34 @@
+#include <GL/glut.h>
+
+void display(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+  glColor3f(1.0, 1.0, 1.0);
+  glBegin(GL_POLYGON);
+  glVertex3f(2.0, 4.0, 0.0);
+  glVertex3f(4.0, 4.0, 0.0);
+  glVertex3f(4.0, 6.0, 0.0);
+  glVertex3f(2.0, 6.0, 0.0);
+  glEnd();
+  glFlush();
+}
+
+int main(int argc, char **argv)
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode ( GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH );
+
+  glutInitWindowPosition(100, 100);
+  glutInitWindowSize(300, 300);
+  glutCreateWindow ("Square");
+
+  glClearColor (0.0, 0.0, 0.0, 0.0);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0.0, 10.0, 0.0, 10.0, -1.0, 1.0);
+
+  glutDisplayFunc(display);
+  glutMainLoop();
+
+  return 0;
+}
