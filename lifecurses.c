a4f5a6fb309a7a702ecc3c8c47ad90d59d2573f9
Ong <ong@localhost.localdomain>
2017-11-19 19:45:05 -0700

    testing

--- lifecurses.c
+++ lifecurses.c
@@ -7,18 +7,27 @@
 //for 80x24
 #define WORLDY 80
 #define WORLDX 24
-
-
+//living and birth values 
+#define BIRTH -32768
+#define LIVING 16384 
 //array where life will grow in a 80x24 screen 
-char world[WORLDY,WORLDX];
+short world[WORLDY,WORLDX];
+//short gen;
 
+void three_seed()
+{
+  int i;
+  for (i = 0; i < 3; i++)
+    for( j = 0; j < 3; j++)
+      world[i,j] = 'X';
+}
 void start_world()
 {
   int i, j;
   initscr();
   for ( i = 0; i < WORLDY; i++)
     for( j = 0; j < WORLDX; j++)
-      world[i,j] = ' ';
+      world[i,j] = 0;
 }
 
 void exit_scr()
@@ -26,8 +35,27 @@ void exit_scr()
   endwin();
 }
 
-void check_nc (int y,int x)
+//checks one space around returns dead or alive 
+void check_around (int y, int x)
 {
-  int i, j;
-  move( y , x );
+  //ncell is neighboring cell it will increase with every neighboring cell
+  int i, j, ncell = 0;
+  for (i = 0; i < 3; i++)
+    for( j = 0; j < 3; j++)
+      {
+      //checks the short 16384 = 0100000000000000
+      //is if it greater then we have an alive cell
+      if(world[ y + i, x + j]>=LIVING)
+	ncell++;
+      else
+	continue;
+      }
+  if (ncell > 1 || ncell < 4)
+    world[ y , x ]+=BIRTH;
+ 
+}
+
+void check_world()
+{
+  
 }
